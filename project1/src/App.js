9d7af8889043c7894c91ef3ac7ae9207dd8a6dbd
lucasahlgren <35539195+lucasahlgren@users.noreply.github.com>
2019-01-20 19:57:11 +0100

    react-vis select person working

--- project1/src/App.js
+++ project1/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 /* React-vis */
 import '../node_modules/react-vis/dist/style.css';
-import {XYPlot, LineSeries} from 'react-vis';
+import {XYPlot, LineSerie, VerticalGridLines, HorizontalGridLines, XAxis, YAxis, LineSeries, Legends, VerticalBarSeries} from 'react-vis';
 
 import BubbleChart from '@weknow/react-bubble-chart-d3';
 import data from './assets/data.json';
@@ -13,24 +13,61 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      type: "How would you rate your programming skills?",
+      person: {name: "", data: [{x: 0, y: 8}]}
+
     }
     this.filterData = this.filterData.bind(this)
+    this.cleanData = this.cleanData.bind(this)
     this.renderData = this.renderData.bind(this)
+    this.selectPerson = this.selectPerson.bind(this)
   }
 
 
   componentWillMount() {
+    this.cleanData()
+  }
+
+  componentDidMount(){
     this.renderData()
   }
 
-  renderData(){
+  cleanData(){
     console.log(data);
-    var filteredArray = [];
-    data.map(row => filteredArray.push({ label: row["What is your first and last name?"], value: row[this.state.type] }))
+    var cleanedData = [];
+    data.map(person => cleanedData.push({ name: person["What is your first and last name?"],
+    data: [{x: "Visualization", y: person["How would you rate your Information Visualization skills?"]},
+      {x: "Statistics", y: person["How would you rate your statistical skills?"]},
+      {x: "Math", y: person["How would you rate your mathematics skills?"]},
+      {x: "Drawing", y: person["How would you rate your drawing and artistic skills?"]},
+      {x: "Coding", y: person["How would you rate your programming skills?"]},
+      {x: "UX", y: person["How would you rate your user experience evaluation skills?"]}]
+  }))
+     
+     console.log(cleanedData)
+     var options = cleanedData.map(person => <option key={person["name"]} value={person["name"]}>{person["name"]}</option>)
+     console.log(options)
+    this.setState({data: cleanedData, options: options, person: cleanedData[0]})
+  }
 
+  renderData(){
+  
+    var filteredArray = [];
+    data.map(row => filteredArray.push({ x: "Programming skills", y: row[this.state.type] }))
     console.log(filteredArray);
-    this.setState({ data: filteredArray })
+    this.setState({ filterData: filteredArray })
+
+
+  }
+
+  selectPerson(person){
+    var personList = this.state.data;
+    for (var index = 0; index < personList.length; index++){
+      if(person === personList[index]["name"]){
+        console.log("Selected person: " + personList[index]["name"])
+        this.setState({person: personList[index]})
+        break;
+      }
+    }
   }
 
 
@@ -40,12 +77,12 @@ class App extends Component {
 
   filterData(e){
     console.log(e.target.value)
-    this.setState({type: e.target.value})
+    this.selectPerson(e.target.value)
     this.renderData()
   }
 
   render() {
-    this.bubbleClick = (label) => {
+    /*this.bubbleClick = (label) => {
       console.log("Custom bubble click func")
     }
     this.legendClick = (label) => {
@@ -55,20 +92,41 @@ class App extends Component {
       var newList = this.state.data.filter(data => data.label === label);
       this.setState({ data: newList });
     }
+    */
+    
+
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Visualize Student Group </h1>
+          <h1>Visualize Student Groups</h1>
         </header>
         <div>
-        <select onChange={this.filterData} value={this.state.type}>
-          <option value="How would you rate your programming skills?">Programming skills</option>
-          <option value="How would you rate your mathematics skills?">Mathematics skills</option>
-          <option value="How would you rate your human-computer interaction programming skills?">Interaction programming skills</option>
-          <option value="How would you rate your drawing and artistic skills?">Drawing and artistic skills</option>
+        <select onChange={this.filterData} value={this.state.person.name}>
+          {this.state.options}
         </select>
         </div>
-        <BubbleChart
+        <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <h1>Welcome</h1>  
+            <p>With this visualization tools you can search and look up the skills of the persons who are taken the course this year.</p>
+          </div>
+        </div>
+        </div>
+<div className="container col-6">
+ <XYPlot width={600} height={300} yDomain={[0, 10]}  xType="ordinal">
+          <VerticalGridLines />
+          <HorizontalGridLines />
+          <XAxis  />
+          <YAxis tickValues={[0,1,2,3,4,5,6,7,8,9,10]} />
+          <VerticalBarSeries animation data={this.state.person["data"]} />
+        </XYPlot>
+        </div>
+
+ 
+
+
+        {/*<BubbleChart
 
 
           width={1400}
@@ -98,7 +156,7 @@ class App extends Component {
           legendClickFun={this.legendClick}
           data={this.state.data}
         />
-
+        */}
       </div>
 
     );
