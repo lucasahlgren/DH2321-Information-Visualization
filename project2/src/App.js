cd617f82786fe82dcb65cfee7839d5ca91f6ea5b
lucasahlgren <35539195+lucasahlgren@users.noreply.github.com>
2019-02-10 00:34:47 +0100

    map update

--- project2/src/App.js
+++ project2/src/App.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import WorldMap from './worldMap/worldMap';
+import Fade from 'react-reveal/Fade';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <div className="container-fluid">
+      <div className="row mx-auto">
+      <div className="col-12 mx-auto">
+      <header>
+        <h1 className="header p-3 text-center">World value survey</h1>
+      </header>
+      </div>
+      </div>
+      </div>
+      <Fade delay={500}>
         <WorldMap />
+        </Fade>
       </div>
     );
   }
